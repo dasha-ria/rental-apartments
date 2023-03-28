3dd929c9c056b18cb046855ef460dc09bf1b5290
Daria <dariastoicad@gmail.com>
2023-03-28 13:30:49 +0200

    booting up

---
+++ next-js/src/pages/_app.tsx
@@ -0,0 +1,6 @@
+import 'rental-apartments/styles/globals.css'
+import type { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
